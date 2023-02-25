49853f072e8d2d5dc7e9dee54a9add7cd58b1dc6
Arjun Kar <arjunkar0@gmail.com>
2023-02-25 18:19:56 +0530

    Reward-to-go

--- vpg.py
+++ vpg.py
@@ -6,16 +6,24 @@ Does not yet implement reward-to-go or generalized advantage estimation.
 Parallelization is automated by the gymnasium package, maintained
 by the Farama Foundation.
 Each batch of batch_size rollouts is played simultaneously before the
-policy is updated.
+policy is updated in a single "episode".
 
 CartPole-v1 Results:
 This code, with weights given by simply the total rollout reward,
-can produce a policy within 250 training episodes (policy updates)
+can produce a policy within 200-250 training episodes (policy updates)
 which achieves over 10 test rollouts an average reward 
-of 495.4 with variance 3.8.
+of 495.4 with variance (torch.var) 3.8.
 This qualifies as a solution to CartPole-v1 as defined by the
 Farama Foundation, which asks for an average reward of just 475
 out of the truncation limit of 500.
+
+With reward-to-go, the required episodes drop to less than 50-100,
+although the variance of the resulting model is larger.
+The reward-to-go implementation can achieve a mean reward of 490.8
+with variance 22.4.
+In 50 episodes, the average reward is already over 400.
+This is an improvement over the total reward weight, which sits at
+just 140 average reward after 50 episodes.
 """
 
 import torch
@@ -66,10 +74,16 @@ def vpg_loss(obs_seq, act_seq, rew_seq, mask_seq):
     policy = torch.distributions.categorical.Categorical(logits=logits_seq)
     log_prob_seq = policy.log_prob(act_seq)
 
-    weights = (rew_seq*not_mask).sum(dim=0, keepdim=True)
+    # Total reward weighting:
+    # weights = (rew_seq*not_mask).sum(dim=0, keepdim=True)
+    # Reward-to-go weighting:
+    total_weights = (rew_seq*not_mask).sum(dim=0, keepdim=True)
+    weights = total_weights - (rew_seq*not_mask).cumsum(dim=0)
+
     log_probs = log_prob_seq * not_mask
 
-    return -(log_probs * weights).sum() / batch_size, weights.sum() / batch_size
+    return -(log_probs * weights).sum() / batch_size, total_weights.sum() / batch_size
+    # Depending on the weights, make sure to report _ , average reward here
 
 
 episodes = 1000
@@ -96,12 +110,16 @@ def test():
     logits_net.eval()
     obs, act, rew, mask = play_episode()
     loss, avg_rew = vpg_loss(obs, act, rew, mask)
-    print(f"Test loss = {loss:>7f}")
-    print(f"Average test reward = {avg_rew.item():>7f}\n")
+    # print(f"Test loss = {loss:>7f}")
+    # print(f"Average test reward = {avg_rew.item():>7f}\n")
+    return avg_rew
 
 train()
+avg_rew_list = []
 for _ in range(10):
-    test()
+    avg_rew_list.append(test())
 
-
-
+avgs = torch.tensor(avg_rew_list)
+print(avgs)
+print(avgs.mean())
+print(avgs.var())
